ba4d61c5ac9f2cd83edd7f1099239c585cffc905
dal gobbo theo <theo.dal_gobbo@edu.univ-fcomte.fr>
2024-10-08 10:14:33 +0200

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Route pour servir le fichier HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en écoute sur le port ${port}`);
+});
